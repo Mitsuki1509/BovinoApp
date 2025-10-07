4dc1df163d84c828b9cc25f17313a26e09be194c
Mitsuki1509 <sherlyescotoe@gmail.com>
2025-10-07 16:25:12 -0600

    tailwind config and initialize backend with express

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,11 @@
-import { useState } from 'react'
 import './App.css'
-import './fonts/fonts.css'
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
-    <>
-   
-    <div className="login-container">
-      <div className="logo">
-        <h1>Agrícola <b>San Pablo S.A.</b></h1>
-      </div>
-      
-      <div className="login-inputs">
-        <label htmlFor="email">Email</label>
-        <input type="email" placeholder="email@ejemplo.com" id="email"/>
-      
-        <label htmlFor="password">Contraseña</label>
-        <input type="password" placeholder="contraseña" id="password"/>
-      
-        <label className="checkbox">
-          <input type="checkbox" name="save_password"/>
-          Guardar contraseña
-        </label>
-        
-        <button className="restore">
-          ¿Olvidó su contraseña?
-        </button>
-        
-        <button className="login">
-          Iniciar sesión
-        </button> 
-      </div>
-      </div>
-      
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-3xl font-bold text-white">¡Tailwind funciona!</h1>
+    </div>
+  );
 }
 
 export default App
